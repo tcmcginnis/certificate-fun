3215e6c62e550f8e7d66e83018bd3f21496d92e8
root <root@bastion.mclabs.us>
2022-10-05 14:26:49 -0400

    Improved sed command

--- pre-flight_certs_check.sh
+++ pre-flight_certs_check.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+#set -x
 
-OPENSHIFT_CONFIG="ocp4.10.13/install-config-MIRROR-SAVE.yaml"
+OPENSHIFT_CONFIG="/root/ocp4.10.13/install-config-MIRROR-SAVE.yaml"
 REQUIRED_ISSUER="C = US, ST = Ohio, L = Cleveland, O = McLabs.us, OU = lab, CN = bastion.mclabs.us, emailAddress = tom@mcginnis.nu"
 REQUIRED_ISSUER="l = US, ST = Ohio, L = Cleveland, O = McLabs.us, OU = lab, CN = bastion.mclabs.us, emailAddress = tom@mcginnis.nu"
 
-BEGIN=$(grep -n "\-\-\-\-\-BEGIN CERTIFICATE\-\-\-\-\-" $OPENSHIFT_CONFIG | awk -F: '{print $1}')
-END=$(grep -n "\-\-\-\-\-END CERTIFICATE\-\-\-\-\-" $OPENSHIFT_CONFIG | awk -F: '{print $1}')
-
-CERT_ISSUER=$(sed -n "$BEGIN,$END""p;" ocp4.10.13/install-config-MIRROR-SAVE.yaml|sed 's/  //g'|openssl x509 -text|grep "Issuer:"|awk -F"Issuer: " '{print $2}')
+CERT_ISSUER=$(sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' $OPENSHIFT_CONFIG|sed 's/  //g'|openssl x509 -text|grep "Issuer:"|awk -F"Issuer: " '{print $2}')
 
 echo "$CERT_ISSUER"
 
